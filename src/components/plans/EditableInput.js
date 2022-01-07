7483b342650575f3b69a3b0e39ff405b359f9eca
Randy Legair <randy@savagerydigital.com>
2022-01-07 15:05:06 -0500

    adding images for README.md

--- src/components/plans/EditableInput.js
+++ src/components/plans/EditableInput.js
@@ -2,15 +2,16 @@ import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import GeneralModal from '../modal/GeneralModal'
 
-function EditableInput({ text, tag = "p", classNames, inputType = 'text' }) {
+function EditableInput({ text, tag = "p", classNames, inputType = 'text', updateFunction }) {
     const [isEditing, setIsEditing] = useState(false)
     // TODO I have two variables for the two states of text input, in the input and in the tag. Is this neccessary? would it be better to use the 'text' var that already exists and update that? 
     const [textInputContent, setTextInputContent] = useState(text)
     const [textContent, setTextContent] = useState(text)
     const [discardModalShowing, setDiscardModalShowing] = useState(false)
     const [confirmModalShowing, setConfirmModalShowing] = useState(false)
-    const [confirmOrDiscardModalShowing, setConfirmOrDiscardModalShowing] = useState(false)
     const [lastFocusedInput, setLastFocusedInput] = useState()
+
+    // TODO So this was placed here so that the discard menu didn't pop up when the confirm changes modal took focus when the modal buttons were clicked. 
     const [saving, setSaving] = useState(false)
 
     const toggleEditing = () => {
@@ -31,56 +32,25 @@ function EditableInput({ text, tag = "p", classNames, inputType = 'text' }) {
         }
 
     }
-    const handleInput = (event) => {
-        // TODO I think I'll need to reset this when the value is discarded, but I've got spaghetti brain right now
-        event.target.parentNode.dataset.replicatedValue = event.target.value
 
-        const validText = validateInput(event.target.value)
-        console.log(event.key)
-    }
     const confirmTextInput = () => {
         // TODO There is a logic issue here... If I discard changes when confirming, the input box no longer has listeners for focus out or input.
-        
+
         toggleEditing()
         setTextContent(textInputContent)
-        if (inputType === 'textarea') {
-            hideConfirmOrDiscardModal()
-        } else {
-            hideConfirmModal()
-        }
-
-
-        //TODO I've already forgotten why I need this saving flag...
-        setSaving(false)
-
+        updateFunction(textInputContent)
+        hideConfirmModal()
     }
     const confirmDiscard = () => {
         toggleEditing()
-        if (inputType === 'textarea') {
-            hideConfirmOrDiscardModal()
-        } else {
-            hideDiscardModal()
-        }
+        hideDiscardModal()
+
     }
     const handleClick = () => {
         toggleEditing()
     }
     const handleBlur = (event) => {
-        setTextInputContent()
-        if (inputType === 'textarea') {
-            // TODO this "textHasChanged" variable is repeaetd, there should be a way to make this check consistent, Maybe move it to a function? Or not make the call at all? 
-            const textHasChanged = !(event.target.value == textInputContent)
-            if (textHasChanged) {
-                const validText = validateInput(event.target.value)
-                setTextInputContent(validText)
-                showConfirmOrDiscardModal()
-            } else {
-                toggleEditing()
-                hideConfirmOrDiscardModal()
-            }
-        } else {
-            showDiscardModal(event)
-        }
+        showDiscardModal(event)
     }
     // TODO I have found myself repeating this modal pattern alot, maybe it's time to move this to a global state? and just have a handful of modal elements? 
     const showDiscardModal = (event) => {
@@ -94,7 +64,6 @@ function EditableInput({ text, tag = "p", classNames, inputType = 'text' }) {
                 if (!discardModalShowing) {
                     setDiscardModalShowing(true)
                     setLastFocusedInput(event.target)
-
                 }
 
             }
@@ -105,7 +74,6 @@ function EditableInput({ text, tag = "p", classNames, inputType = 'text' }) {
         setDiscardModalShowing(false)
         if (!saving) {
             lastFocusedInput.focus()
-
         }
     }
     const showConfirmModal = () => {
@@ -116,35 +84,20 @@ function EditableInput({ text, tag = "p", classNames, inputType = 'text' }) {
         }
     }
     const hideConfirmModal = () => {
+        setSaving(false)
         setConfirmModalShowing(false)
     }
-    const showConfirmOrDiscardModal = (textValue) => {
-        if (!confirmOrDiscardModalShowing) {
-            setConfirmOrDiscardModalShowing(true)
-        }
-    }
-    const hideConfirmOrDiscardModal = () => {
-        setConfirmOrDiscardModalShowing(false)
-    }
+
     const textDisplay = React.createElement(tag, { className: classNames, onClick: handleClick }, textInputContent)
-    const textInput = inputType === 'textarea' ? <textarea defaultValue={textContent} autoFocus onInput={handleInput} onBlur={handleBlur}></textarea> : <input defaultValue={textContent} autoFocus type={inputType} onKeyPress={handleKeyPress} onBlur={handleBlur}></input>
+    const textInput = <input defaultValue={textContent} autoFocus type='text' onKeyPress={handleKeyPress} onBlur={handleBlur}></input>
     const discardConfirmationModal = <GeneralModal heading={"Discard changes?"} confirmText='Discard' confirmAction={confirmDiscard} denyAction={hideDiscardModal}></GeneralModal>
     const confirmModal = <GeneralModal heading={"Save changes?"} confirmAction={confirmTextInput} denyAction={hideConfirmModal}></GeneralModal>
-    const otherActions = [
-        {
-            label: 'Discard',
-            actionFunction: confirmDiscard,
-            classNames: 'button-alternate mid'
-        }
-    ]
-    const confirmOrDiscardModal = <GeneralModal heading={"Save changes?"} confirmAction={confirmTextInput} denyAction={hideConfirmOrDiscardModal} otherActions={otherActions}></GeneralModal>
     return (
         <div className={inputType === 'textarea' ? 'responsive-textarea-wrapper' : ''}>
             {isEditing ? textInput : textDisplay}
             {/* TODO This feels sloppy, is there a better way?  */}
             {discardModalShowing && discardConfirmationModal}
             {confirmModalShowing && confirmModal}
-            {confirmOrDiscardModalShowing && confirmOrDiscardModal}
         </div>
     )
 }
--- src/components/plans/tasks/TaskItem.js
+++ src/components/plans/tasks/TaskItem.js
@@ -2,17 +2,25 @@ import React from 'react'
 import TaskProgressBar from '../TaskProgressBar'
 import StepList from './StepList'
 import EditableInput from '../EditableInput'
+import {useDispatch} from 'react-redux'
+import { updateProperty } from '../../../features/plans/plansSlice'
 
 function TaskItem({task, planId, showModalFunction}) {
+
+    const dispatch = useDispatch()
 
     const handleRemove = (event)=>{
         // TODO Should really have a confirmation modal here
         showModalFunction([planId], task._id,"task")
 
     }
+    const handleTitleUpdate = (title) =>{
+        const taskUpdateObject = {title}
+        dispatch(updateProperty([planId], task._id, taskUpdateObject,"task"))
+    }
     return (
         <div className="task-item panel">
-            <EditableInput classNames={'item-title xsmall-margin-bottom'} text={task.title} ></EditableInput><span className="delete-item-button" onClick={handleRemove}>x</span>
+            <EditableInput updateFunction={handleTitleUpdate} classNames={'item-title xsmall-margin-bottom'} text={task.title} ></EditableInput><span className="delete-item-button" onClick={handleRemove}>x</span>
 
             <p className={"task " + task.priority}>{task.priority}</p>   
 
